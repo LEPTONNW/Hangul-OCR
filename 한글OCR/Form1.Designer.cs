c1b68c45dee9b0077acdc670160b668485cb296e
Round Robin <kac4484@gmail.com>
2021-05-20 21:33:49 +0900

    Add files via upload

---
+++ 한글OCR/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace 한글OCR
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.PathBnt = new System.Windows.Forms.Button();
+            this.PathText = new System.Windows.Forms.TextBox();
+            this.START = new System.Windows.Forms.Button();
+            this.ResultBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(27, 28);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(390, 239);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // PathBnt
+            // 
+            this.PathBnt.Location = new System.Drawing.Point(440, 27);
+            this.PathBnt.Name = "PathBnt";
+            this.PathBnt.Size = new System.Drawing.Size(145, 23);
+            this.PathBnt.TabIndex = 1;
+            this.PathBnt.Text = "불러오기";
+            this.PathBnt.UseVisualStyleBackColor = true;
+            this.PathBnt.Click += new System.EventHandler(this.PathBnt_Click);
+            // 
+            // PathText
+            // 
+            this.PathText.Location = new System.Drawing.Point(591, 29);
+            this.PathText.Name = "PathText";
+            this.PathText.Size = new System.Drawing.Size(180, 21);
+            this.PathText.TabIndex = 2;
+            // 
+            // START
+            // 
+            this.START.Font = new System.Drawing.Font("굴림", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.START.Location = new System.Drawing.Point(440, 70);
+            this.START.Name = "START";
+            this.START.Size = new System.Drawing.Size(331, 99);
+            this.START.TabIndex = 3;
+            this.START.Text = "판독";
+            this.START.UseVisualStyleBackColor = true;
+            this.START.Click += new System.EventHandler(this.START_Click);
+            // 
+            // ResultBox
+            // 
+            this.ResultBox.Location = new System.Drawing.Point(27, 324);
+            this.ResultBox.Multiline = true;
+            this.ResultBox.Name = "ResultBox";
+            this.ResultBox.Size = new System.Drawing.Size(744, 197);
+            this.ResultBox.TabIndex = 4;
+            this.ResultBox.TextChanged += new System.EventHandler(this.ResultBox_TextChanged);
+            this.ResultBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.ResultBox_KeyDown);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 309);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "판독결과";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(438, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "사진경로";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 567);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ResultBox);
+            this.Controls.Add(this.START);
+            this.Controls.Add(this.PathText);
+            this.Controls.Add(this.PathBnt);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button PathBnt;
+        private System.Windows.Forms.TextBox PathText;
+        private System.Windows.Forms.Button START;
+        private System.Windows.Forms.TextBox ResultBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ 한글OCR/Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tesseract;
+
+namespace 한글OCR
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public static List<String> DIC = new List<String>();
+        public static bool BlockFillter;
+        public static int TIME = 0;
+        public static Bitmap img;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DIC.Add("페미");
+            DIC.Add("꽤미");
+            DIC.Add("한남");
+            DIC.Add("자들자들");
+            DIC.Add("여성혐오");
+            DIC.Add("냄저");
+            DIC.Add("냄져");
+            DIC.Add("좆돌");
+            DIC.Add("젠더");
+            DIC.Add("젠더 감수성");
+            DIC.Add("젠더감수성");
+            DIC.Add("흉자");
+            DIC.Add("자이루");
+            DIC.Add("모르면 배우세요");
+            DIC.Add("모르면배우세요");
+            DIC.Add("모르면공부하세요");
+            DIC.Add("모르면 공부하세요");
+            DIC.Add("모르면 외우세요");
+            DIC.Add("모르면외우세요");
+            DIC.Add("재기해");
+            DIC.Add("재기");
+            DIC.Add("부랄발광");
+            DIC.Add("좆팔");
+            DIC.Add("조팔");
+            DIC.Add("살남");
+            DIC.Add("걸스캔두애니띵");
+            DIC.Add("걸스캔두에니띵");
+            DIC.Add("캔두에니띵");
+            DIC.Add("캔두 에니띵");
+            DIC.Add("워마드");
+        }
+
+        private void PathBnt_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "JPG  (*.jpg / *.jpeg)|*jpg|PNG (*.png)|*.png|All File (*.*)|*.*";
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                PathText.Text = openFileDialog1.FileName.ToString();
+            }
+
+            pictureBox1.ImageLocation = PathText.Text;
+        }
+
+        private void START_Click(object sender, EventArgs e)
+        {
+            timer1.Interval = 1000;
+            timer1.Start();
+
+
+        }
+
+        private void ResultBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.Control)
+            {
+                if(e.KeyCode == Keys.A)
+                {
+                    this.ResultBox.Focus();
+                    this.ResultBox.SelectAll();
+                }
+            }
+        }
+
+        private void ResultBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        [DllImport("user32.dll")]
+        public static extern void keybd_event(uint vk, uint scan, uint flags, uint extraInfo);
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (TIME == 5)
+            {
+                if (!Directory.Exists("CAP"))
+                {
+                    Directory.CreateDirectory("CAP");
+                }
+                if(File.Exists(@"CAP\K-001.png"))
+                {
+                    File.Delete(@"CAP\K-001.png");
+                }
+                keybd_event((byte)Keys.ControlKey, 0, 0x00, 0);
+                keybd_event((byte)Keys.P, 0, 0x00, 0);
+                keybd_event((byte)Keys.P, 0, 0x02, 0);
+                keybd_event((byte)Keys.ControlKey, 0, 0x02, 0);
+
+
+
+                pictureBox1.ImageLocation = @"CAP\K-001.png";
+                Thread.Sleep(2000);
+                ResultBox.Text = "";
+                //MessageBox.Show("1");
+                try
+                {
+                    img = new Bitmap(pictureBox1.Image);
+                    
+                    var ocr = new TesseractEngine("./tessdata", "kor", EngineMode.Default);
+                    //var ocr = new TesseractEngine("./tessdata", "eng", EngineMode.TesseractAndCube);
+                    var texts = ocr.Process(img);
+                    ResultBox.Text = texts.GetText().ToString();
+
+                    
+                }
+                catch { }
+                string HResult = ResultBox.Text;
+                foreach (string Blockword in DIC)
+                {
+                    BlockFillter = HResult.Contains(Blockword);
+
+                    if (BlockFillter.ToString() == "True")
+                    {
+                        if (!File.Exists("BlockList.txt"))
+                        {
+                            File.Create("BlockList.txt");
+                        }
+                        var ocr2 = new TesseractEngine("./tessdata", "eng", EngineMode.TesseractAndCube);
+                        var texts2 = ocr2.Process(img);
+                        string EResult = texts2.GetText().ToString();
+                        using (StreamWriter BlockList = new StreamWriter("BlockList.txt", true))
+                        {
+                            BlockList.WriteLine(EResult + Environment.NewLine);
+                        }
+
+
+                        break;
+                    }
+                }
+                TIME = 0;
+            }
+            TIME++;
+        }
+    }
+}
